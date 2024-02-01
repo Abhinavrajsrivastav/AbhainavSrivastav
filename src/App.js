6c04fa45f67ba5f3f78c362936d0a6130b39cdfe
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-02-01 20:07:05 +0530

    01-02-2024-002

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import NavBar from './Components/NavBar/NavBar';
+import Home from './Components/Home/Home'
+import Work from './Components/Works/Work'
+
 
 const App = () => {
   return (
+    <>
     <NavBar />
+    <Home />
+    <Work />
+    </>
   )
 }
 
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import './Home.css';
+
+const Home = () => {
+  const [Deck, setDeck] = useState(null);
+
+  useEffect(() => {
+    const importDeck = async () => {
+      // Dynamic import using the dynamic import() syntax
+      const dynamicImport = await import('./skillsCards/DeckCards');
+      setDeck(() => dynamicImport.default);
+    };
+
+    importDeck();
+  }, []);
+
+  return (
+    <div className="Home-Container">
+      <div className="about-me">
+        <div className="Head-About">
+          <>
+            <span className="highlight">Coding</span> &amp;
+            <br className="break-line" />
+            <span className="highlight">Storytelling</span>:
+            <br /> That's what I do!
+          </>
+        </div>
+        <div className="Profession">
+          <p>Engineering</p>
+          <p>Educating</p>
+          <p>Entertaining</p>
+        </div>
+        <div className="My-Thought">
+          <span>I turn ideas into reality with the power of software engineering, and I share my stories as a content <br />creator.</span>
+        </div>
+        <div className="Chat">
+          <button className="Chat-Btn button">Let's Chat!</button>
+        </div>
+      </div>
+      <div className="My-Profile">
+        <p className="">🎉🎉</p>
+        {Deck && <Deck />}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
