ac18021f3dd3447da2611a262691c2f635162de1
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-03-20 17:23:15 +0530

    20-03-2024-003

--- src/Components/Footter/Fotter.js
+++ src/Components/Footter/Fotter.js
@@ -26,7 +26,7 @@ const Fotter = () => {
                         <p>General<span style={{color: 'rgb(18, 197, 202)'}}>.</span></p>
                         <a href="/">Home</a>
                         <a href="/About">About</a>
-                        <a href="https://drive.google.com/file/d/1RKXEKqv9a9PpwAcYw84yPLZ5_-KBPN0r/view?usp=drivesdk">Resume</a>
+                        <a href="https://drive.google.com/file/d/1YIZXN_ormH0Iv1HoV0_vbCYWxRNDbJHs/view?usp=drivesdk">Resume</a>
                         <a href="/Contact">Contact</a>
                         <a href="/Achievements">Achievements</a>
                     </div>
