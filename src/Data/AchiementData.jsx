6a79408333444050b3062e98b73aac1325288b23
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-04-13 22:43:08 +0530

    13-04-2024-008

--- src/Data/AchiementData.jsx
+++ src/Data/AchiementData.jsx
@@ -13,8 +13,8 @@ const AchiementData = () => {
         {
             src: "./images/Achivements/leetcode.png",
             gitHub: "",
-            name: "Solved 80+ problems solved at leetdode.",
-            about: "🏆 Top 41% at leetcode",
+            name: "Solved 100+ problems solved at leetdode.",
+            about: "",
             link: "https://leetcode.com/Abhinav254/",
             techs: ["Java", "DSA", "Competitive Programming", "C", "Coding Contest"]
         },
@@ -22,14 +22,14 @@ const AchiementData = () => {
             src: "./images/Achivements/50daysleetcode.png",
             gitHub: "",
             name: "Consistent 50+ days CP coding at leetdode.",
-            about: "🏆 Contest Rating - 1502(*1)",
+            about: "🏆 Max Contest Rating - 1502",
             link: "https://leetcode.com/Abhinav254/",
             techs: ["Java", "DSA", "Competitive Programming", "Coding Contest"]
         },
         {
             src: "./images/Achivements/github.png",
             gitHub: "",
-            name: "700+ contributions on GitHub.",
+            name: "800+ contributions on GitHub.",
             about: "🏆 Achived 12+ stars",
             link: "https://github.com/Abhinavrajsrivastav",
             techs: ["Core Java", "DSA", "Competitive Programming", "Contribution", "Open Source", "JavaScript", "React.js", "Node.js", "Spring Boot"]
@@ -45,7 +45,7 @@ const AchiementData = () => {
         {
             src: "./images/Achivements/Java_Pw.png",
             gitHub: "",
-            name: "Java with DSA and System Design.",
+            name: "Java with DSA and System Design Pw Skills",
             about: "",
             link: "https://learn.pwskills.com/certificate/15a6646c-67ba-4222-b36b-0e732e9c100c",
             techs: ["Java", "DSA", "Competitive Programming", "SQL", "MongoDB", "Spring Boot", "JDBC", "DataBase", "System Design", "Object Oriented Programming"]
@@ -57,7 +57,7 @@ const AchiementData = () => {
         {
             src: "./images/Achivements/Web_Development_Bootcamp.png",
             gitHub: "",
-            name: "Web Development Bootcamp Udemy",
+            name: "Web Development Bootcamp at Udemy",
             about: "",
             link: "https://www.udemy.com/certificate/UC-c18bbb12-d072-4ac8-85c5-b5cc1c0fc43a/",
             techs: ["HTML", "CSS", "JavaScript", "Bootstrap", "React.js", "Node.js", "Express.js", "SQL", "MongoDB"]
@@ -65,7 +65,7 @@ const AchiementData = () => {
         {
             src: "./images/Achivements/Jav_Basics.png",
             gitHub: "",
-            name: "Problem Solving with Java",
+            name: "Problem Solving with Java at Hackerrank",
             about: "",
             link: "https://www.hackerrank.com/profile/CSME2A_b1531174",
             techs: ["Java", "Problem Solving"]
@@ -73,7 +73,7 @@ const AchiementData = () => {
         {
             src: "./images/Achivements/Problem_solving.png",
             gitHub: "",
-            name: "Problem Solving with Java",
+            name: "Problem Solving with Java at Hackerrank",
             about: "",
             link: "https://www.hackerrank.com/profile/CSME2A_b1531174",
             techs: ["Java", "C", "Problem Solving"]
@@ -81,7 +81,7 @@ const AchiementData = () => {
         {
             src: "./images/Achivements/SQL_Basics.png",
             gitHub: "",
-            name: "SQL Basics Certified",
+            name: "SQL Basics Certified at Hackerrank",
             about: "",
             link: "https://www.hackerrank.com/profile/CSME2A_b1531174",
             techs: ["SQL", "Problem Solving"]
@@ -89,7 +89,7 @@ const AchiementData = () => {
         {
             src: "./images/Achivements/Java_Script_Essential.png",
             gitHub: "",
-            name: "Javascript Essential 1.0 Cisco",
+            name: "Javascript Essential at Cisco",
             about: "",
             link: "https://www.credly.com/badges/59f9792a-a875-4d85-9652-0ce7712868ca",
             techs: ["JavaScript", "Problem Solving", "DSA", "JavaScript fundamentals"]
@@ -97,7 +97,7 @@ const AchiementData = () => {
         {
             src: "./images/Achivements/Python_Essential.png",
             gitHub: "",
-            name: "Python Essential 1.0 Cisco",
+            name: "Python Essential at Cisco",
             about: "",
             link: "https://www.credly.com/users/abhinav-raj-srivastav/badges",
             techs: ["Python Basics", "Fundamentals"]
