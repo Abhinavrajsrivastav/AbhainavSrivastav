11ba120d41352b53684749e6912e5e316889e77d
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-05-05 00:48:17 +0530

    05-05-2024-002

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -7,26 +7,26 @@ function Skills() {
       <div className="languages">
         <p>Languages</p>
         <div className="skills-container">
-          <img src="./Images/Skills/java.png" alt="" />
-          <img src="./Images/Skills/js-file.png" alt="" />
-          <img src="./Images/Skills/letter-c.png" alt="" />
-          <img src="./Images/Skills/python-file.png" alt="" />
+          <img src="./images/Skills/java.png" alt="" />
+          <img src="./images/Skills/js-file.png" alt="" />
+          <img src="./images/Skills/letter-c.png" alt="" />
+          <img src="./images/Skills/python-file.png" alt="" />
         </div>
       </div>
       <div className="developments">
         <p>Developments</p>
         <div className="skills-container">
-        <img src="./Images/Skills/science.png" alt="" />
-        <img src="./Images/Skills/spring.png" alt="" />
-         <img src="./Images/Skills/programing.png" alt="" />
+        <img src="./images/Skills/science.png" alt="" />
+        <img src="./images/Skills/spring.png" alt="" />
+         <img src="./images/Skills/programing.png" alt="" />
         </div>
       </div>
       <div className="coreSubjects">
         <p>Core Subjects</p>
         <div className="skills-container">
-           <img src="./Images/Skills/structure.png" alt="" />
-          <img src="./Images/Skills/settings.png" alt="" />
-          <img src="./Images/Skills/database.png" alt="" />
+           <img src="./images/Skills/structure.png" alt="" />
+          <img src="./images/Skills/settings.png" alt="" />
+          <img src="./images/Skills/database.png" alt="" />
         </div>
       </div>
       <div className="tools">
