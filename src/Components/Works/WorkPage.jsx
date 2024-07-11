7a9f3335c3b4d924ae60a3942a0535b7db75051a
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-07-11 18:45:30 +0530

    11-07-2024-002

--- src/Components/Works/WorkPage.jsx
+++ src/Components/Works/WorkPage.jsx
@@ -11,9 +11,9 @@ const WorkPage = () => {
           <span className="Page-Text">Coding Projects<span>.</span></span><br />
           <span className="Page-Text-description">Here are some of the projects I've worked on, utilizing a variety of technologies and frameworks.</span>
         </div>
-        
+
         <div className="Page-Cards">
-          <CardPage src={"./images/Works/educome.png"} gitHub={"https://github.com/Abhinavrajsrivastav/pacifics-path"} name={"Educome"} about={"A platform for engineers"} link={"https://abhinavrajsrivastav.github.io/pacifics-path/"} toolImg={["react.png"]} tech={["React", "GCP", "Gemini 1.5", "Firebase","MySql"]}  className="animated" />
+          <CardsPage src={"./images/Works/educome.png"} gitHub={"https://github.com/Abhinavrajsrivastav/pacifics-path"} name={"Educome"} about={"A platform for engineers"} link={"https://abhinavrajsrivastav.github.io/pacifics-path/"} toolImg={["react.png"]} tech={["React", "GCP", "Gemini 1.5", "Firebase","MySql"]}  className="animated" />
           <CardsPage src={"./images/Works/AyodhayaDham.png"} gitHub={"https://github.com/Abhinavrajsrivastav/ADHM"} name={"Ayodhya Dham"} about={"Ayodhya Dham, is a tour guide website. Help you to make your Ayodhya visit comfortable and safe."} link={"https://ayodhyadham.vercel.app/"} toolImg={["react.png"]} tech={["React","Google cloud console","Javascript"]}/>
           <CardsPage src={"./images/Works/hindi.png"} gitHub={"https://github.com/Abhinavrajsrivastav/React-Projects/tree/master/Musics/hinlish"} name={"HindLish"} about={"Hindlish, is a Musics player with some of the best Hindi and English songs"} link={"https://healsync.onrender.com/"} toolImg={["react.png"]} tech={["React"]}/>
           <CardsPage src={"./images/Works/yts.png"} gitHub={"https://github.com/Abhinavrajsrivastav/SpringBootProjects/tree/main/moviefinder"} name={"Movie Finder"} about={"Improving YouTube-Search Search Responses, Enhance user experiences"} link={"https://www.linkedin.com/posts/abhinav-raj-srivastav-599aaa1b2_springboot-javascripts-java-activity-7155206688197758977-9tIt?utm_source=share&utm_medium=member_desktop"} toolImg={["react.png", "leaf.png"]} tech={["React","Spring Boot"]}/>
