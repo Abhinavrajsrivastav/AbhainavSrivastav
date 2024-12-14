219e96ca3f004f6680bed2a41d43371ac2cecdfb
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-12-14 23:59:27 +0530

    14-12-2024-001

--- src/Components/Works/WorkPage.jsx
+++ src/Components/Works/WorkPage.jsx
@@ -22,6 +22,7 @@ const WorkPage = () => {
           {/* <CardsPage src={"./images/Works/Better_Health.png"} gitHub={"https://github.com/Abhinavrajsrivastav/Multi-Step-Forms"} name={"Better Health"} about={"Get a quick with health checkup with, Quiz App."} link={"http://quiz-app-health-01.s3-website.ap-south-1.amazonaws.com/"} toolImg={["react.png"]} tech={["React"]}/> */}
           <CardsPage src={"./images/Works/weather.png"} gitHub={"https://github.com/Abhinavrajsrivastav/SpringBootProjects/tree/main/WhetherApp/demo"} name={"Weather App"} about={"Get Real-time weather updates of a place."} link={"https://www.linkedin.com/posts/abhinav-raj-srivastav-599aaa1b2_javadevelopment-springboot-javascripts-activity-7149425500443017216-ut8D?utm_source=share&utm_medium=member_desktop"} toolImg={["react.png", "leaf.png"]} tech={["React","Spring Boot"]}/>
           {/* <CardsPage src={"./images/Works/image.png"} gitHub={""} name={"Customer Transactions Dashboard"} about={"My First Power Bi Dashboard. Customer Transaction Analysis."} link={"https://abes365-my.sharepoint.com/:u:/g/personal/abhinav_21b1531174_abes_ac_in/EUsuVCtNi61Hh9FCol3AU50BC8Ng9pPerXvQNn-kL9Vy7A?e=UkclRJ"} toolImg={["powerbi.png"]} tech={["Power Bi"]} className="animated" /> */}
+        
         </div>
       </div>
     </div>
